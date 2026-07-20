d77971cc8c472b832b53c461b0ede97bcb6d645e
Daifou <daifalla.harkat2003@gmail.com>
2026-07-20 12:25:05 +0100

    update payment link with return_url

--- src/App.tsx
+++ src/App.tsx
@@ -35,7 +35,7 @@ function LandingPage() {
   const activePackage = useMemo(() => packages[0], []);
 
   const handleOpenCheckout = () => {
-    window.location.href = 'http://pay.chargily.com/test/payment-links/01kx8tfjcwfe215z33wg8dn262';
+    window.location.href = 'https://sofizpay.com/create-payment-link/?account=GCXLJJ36YWR7HH5NSGENKE62UEJ3ZQ5V6QJTA3RQNPAK5E2XURYA4O2S&amount=700&memo=&return_url=https%3A%2F%2Ffaydbatma.shop%2Fthankyou';
   };
 
   return (
